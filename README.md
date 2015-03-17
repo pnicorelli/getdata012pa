92467f3db10eaac452e76cf08fbf8fa82a188a94
Paolo -Willy- Nicorelli <p.nicorelli@gmail.com>
2015-03-17 17:22:57 +0100

    add text

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # getdata012pa
+
+This is the Peer Assessments of **Getting and Cleaning Data** from [Coursera](https://www.coursera.org/course/getdata).
+
+
+The goal of this homework is to prepare tidy data that can be used for later analysis.
+
+## howTo use it
+
+1. clone this `git clone https://github.com/pnicorelli/getdata012pa.git`
+2. enter it `cd getdata012pa`
+3. download the data set `wget https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip`
+4. unzip the data set `unzip getdata_projectfiles_UCI\ HAR\ Dataset.zip`
+4. launch R `R`
+5. on R load the script `source("run_analysis")`
+6. exec `run_analysis("UCI HAR Dataset/")`
+
+## more info
+- read the [CookBook](CodeBook.md)
--- run_analysis.R
+++ run_analysis.R
@@ -1,12 +1,12 @@
-## Data Description : http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones 
-## Data             : https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip 
+## Data Description : http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones
+## Data             : https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
 ## Data it's about 60Mb
 
 run_analysis <- function(dataPath){
 
   require(plyr)
-  
-  ## Open files 
+
+  ## Open files
 
   feature_file <- paste(dataPath, "features.txt", sep = "")
   activity_labels_file <- paste(dataPath, "activity_labels.txt", sep = "")
@@ -17,8 +17,8 @@ run_analysis <- function(dataPath){
   y_test_file  <- paste(dataPath, "test/y_test.txt", sep = "")
   subject_test_file <- paste(dataPath, "test/subject_test.txt", sep = "")
 
-  ## Load data 
-  
+  ## Load data
+
   features <- read.table(feature_file, colClasses = c("character"))
   activity_labels <- read.table(activity_labels_file, col.names = c("ActivityId", "Activity"))
   x_train <- read.table(x_train_file)
@@ -27,7 +27,7 @@ run_analysis <- function(dataPath){
   x_test <- read.table(x_test_file)
   y_test <- read.table(y_test_file)
   subject_test <- read.table(subject_test_file)
-  
+
   ## 1 - Merges the training and the test sets to create one data set.
 
   train_data <- cbind(cbind(x_train, subject_train), y_train)
@@ -35,18 +35,18 @@ run_analysis <- function(dataPath){
   sensor_data <- rbind(train_data, test_data)
   sensor_labels <- rbind(rbind(features, c(562, "Subject")), c(563, "ActivityId"))[,2]
   names(sensor_data) <- sensor_labels
-  
+
   ## 2 - Extracts only the measurements on the mean and standard deviation for each measurement.
-  
+
   data <- sensor_data[,grepl("mean|std|Subject|ActivityId", names(sensor_data))]
-  
+
   ## 3 - Uses descriptive activity names to name the activities in the data set
-  
+
   data <- join(data, activity_labels, by = "ActivityId", match = "first")
   data <- data[,-1]
-  
+
   ## 4 - Appropriately labels the data set with descriptive names.
-  
+
   names(data) <- gsub('\\(|\\)',"",names(data), perl = TRUE)
   names(data) <- make.names(names(data))
 
@@ -60,9 +60,10 @@ run_analysis <- function(dataPath){
   names(data) <- gsub('\\.std',".stdDev",names(data))
   names(data) <- gsub('Freq\\.',"Frequency.",names(data))
   names(data) <- gsub('Freq$',"Frequency",names(data))
-  
+
   ## 5 - From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
-    
+
   tidy = ddply(data, c("Subject","Activity"), numcolwise(mean))
-  write.table(tidy, file = paste(dataPath, "output.txt", sep=""), row.names=FALSE)
+  
+  ##write.table(tidy, file = paste(dataPath, "output.txt", sep=""), row.names=FALSE)
 }
